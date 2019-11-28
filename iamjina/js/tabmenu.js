1b77c078e967ce1136ec1500b20dc0ce496ab536
Jina <jina@Jinaui-MacBookPro.local>
2019-11-28 15:06:48 +0900

    portfolio

--- iamjina/js/tabmenu.js
+++
@@ -1,7 +0,0 @@
-// window.onload =function(){
-//     var contentsMenu = document.getElementsByClassName('dropdown');
-//     var sideMenu = document.getElementsByClassName('tabcontent');
-//     contentsMenu.addEventListener("click", function(){
-//       alert('hey');
-//     })
-// }
