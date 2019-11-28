a1aa46c7b91601301716563cf67b83fe61f79245
Jina <jina@Jinaui-MacBookPro.local>
2019-11-28 14:48:00 +0900

    port

---
+++ HelloIamJina/js/tabmenu.js
@@ -0,0 +1,7 @@
+// window.onload =function(){
+//     var contentsMenu = document.getElementsByClassName('dropdown');
+//     var sideMenu = document.getElementsByClassName('tabcontent');
+//     contentsMenu.addEventListener("click", function(){
+//       alert('hey');
+//     })
+// }
